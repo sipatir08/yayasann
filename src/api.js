b1ea4717cd1667b05857962ddda6178f18f429d9
sipatir08 <fathir080604@gmail.com>
2024-12-15 21:22:57 +0700

    update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const registerUser = async (email, password) => {
     try {
-        const response = await fetch("http://localhost:8082/register", {
+        const response = await fetch("http://localhost:8080/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
